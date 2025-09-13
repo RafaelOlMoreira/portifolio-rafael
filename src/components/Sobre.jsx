3cdd467db8ee2e05fcda0e5d8bee85b2eaca3804
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-09-12 21:06:43 -0300

    refactor(Sobre): Ajustando responsividade 0.3

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -3,26 +3,19 @@ import { TypeAnimation } from 'react-type-animation'
 
 function Sobre() {
   return (
-    <div id='inicio' className='font-poppins pt-4 h-screen w-full bg-[#181818] grid grid-rows-2 items-center justify-items-start
-                                xl:grid-cols-2 xl:pt-60
-                                lg:pt-10
-                                md:pt-7'>
+    <div id='inicio' className='font-poppins pt-17 h-screen w-full bg-[#181818] grid grid-rows-2 items-center justify-items-start
+                                xl:grid-cols-2 xl:pt-30 lg:pt-0 md:pt-2'>
 
-      <div className='xl:ml-15 mx-auto'>
+      <div className='xl:ml-15 mx-auto h-min'>
         <h1 className='fade-in-esquerda block tracking-wide text-[#e8e8e8] text-center text-nowrap object-cover
-                       text-xl 
-                       md:text-2xl 
-                       lg:text-3xl 
-                       xl:text-4xl '>
+                       text-xl md:text-2xl lg:text-3xl xl:text-4xl '>
           Olá! Meu nome é
           <p className='text-4xl font-bold bg-gradient-to-r from-[#03f7e1] to-[#fbfbfb] bg-clip-text text-transparent
                         md:text-5xl lg:text-6xl xl:text-7xl'>
             RAFAEL MOREIRA
           </p>
           <p className='text-xl text-center text-[#e8e8e8]
-                        md:text-2xl 
-                        lg:text-3xl 
-                        xl:text-4xl xl:text-start'>
+                        md:text-2xl lg:text-3xl xl:text-4xl xl:text-start'>
             Sou um Dev
             <TypeAnimation
               sequence={[
@@ -44,16 +37,16 @@ function Sobre() {
           </p>
         </h1>
 
-        <div className='justify-center xl:justify-start flex gap-x-4 fade-in-esquerda'>
-          <button className='flex mt-4 gap-x-1 items-center border-1 p-1.5 px-3.5 rounded-md text-[#e8e8e8] border-[#e8e8e81e] hover:bg-[#e8e8e81e] hover:cursor-pointer'>
+        <div className='block sm:flex gap-x-4 fade-in-esquerda'>
+          <button className='flex mx-auto xl:mx-0 text-sm mt-4 gap-x-1 items-center border-1 p-1.5 px-3.5 rounded-md text-[#e8e8e8] border-[#e8e8e81e] hover:bg-[#e8e8e81e] hover:cursor-pointer'>
             <a href="#projetos">Veja Meus Trabalhos</a>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
             </svg>
           </button>
 
-          <button className='flex mt-4 gap-x-1 items-center border-1 p-1.5 px-3.5 rounded-md text-black bg-[#e8e8e8] border-[#e8e8e81e] hover:bg-[#e8e8e8cb] hover:cursor-pointer'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <button className='flex mx-auto xl:mx-0 text-sm mt-4 gap-x-1 items-center border-1 p-1.5 px-3.5 rounded-md text-black bg-[#e8e8e8] border-[#e8e8e81e] hover:bg-[#e8e8e8cb] hover:cursor-pointer'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
             </svg>
             <a href="#contato">Contate-me</a>
@@ -70,8 +63,8 @@ function Sobre() {
         </svg>
 
         <img src="/fotopersona.png" alt="Minha foto"
-          className="relative items-center max-h-100 rounded-b-full object-cover
-                      xl:max-h-130"
+          className="relative items-center max-h-75 rounded-b-full object-cover
+                      xl:max-h-130 lg:max-h-115 md:max-h-100"
         />
       </div>
 
